8e84e22c124748cd7ae2b282183e434e7404ad79
EmirKosekul <88345933+EmirKosekul@users.noreply.github.com>
2024-05-13 03:29:54 +0300

    Loading screen update

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import re
 from transformers import pipeline
 import random
 
-# summarizer = pipeline("summarization", model="Falconsai/text_summarization")
-# question_generator = pipeline("text2text-generation", model="iarfmoose/t5-base-question-generator")
-# title_generator = pipeline("text2text-generation", model="czearing/article-title-generator")
-# spelling_correction = pipeline("text2text-generation", model="oliverguhr/spelling-correction-english-base")
-# keyword = pipeline("text2text-generation", model="beogradjanka/bart_finetuned_keyphrase_extraction")
-# paraphrase = pipeline("text2text-generation", model="humarin/chatgpt_paraphraser_on_T5_base")
+summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+question_generator = pipeline("text2text-generation", model="iarfmoose/t5-base-question-generator")
+title_generator = pipeline("text2text-generation", model="czearing/article-title-generator")
+spelling_correction = pipeline("text2text-generation", model="oliverguhr/spelling-correction-english-base")
+keyword = pipeline("text2text-generation", model="beogradjanka/bart_finetuned_keyphrase_extraction")
+paraphrase = pipeline("text2text-generation", model="humarin/chatgpt_paraphraser_on_T5_base")
 grammar= pipeline("text2text-generation", model="vennify/t5-base-grammar-correction")
 
 
@@ -123,8 +123,8 @@ def profile():
          texts = user.get('texts', [])
          text_name = request.form['text_name']
          header="Your summary is here!"
-         #result=summarizer(selected_text, max_length=1000, min_length=30, do_sample=False)[0]['summary_text']
-         result=selected_text + " İlk işlem"
+         result=summarizer(selected_text, max_length=1000, min_length=30, do_sample=False)[0]['summary_text']
+        #  result=selected_text + " İlk işlem"
          print("ben tıklandım")
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      elif request.form['action'] == 'second_action':
@@ -132,46 +132,46 @@ def profile():
          texts = user.get('texts', [])
          text_name = request.form['text_name']
          header="A question for you!"
-        #  random_number = random.randint(1, 3)         
-        #  generated_questions = question_generator(selected_text, max_length=50, num_return_sequences=5, num_beams=5, early_stopping=True)
-        #  question=generated_questions[random_number]['generated_text']
-        #  result=question
-         result=selected_text + " İkinci işlem"
+         random_number = random.randint(1, 3)         
+         generated_questions = question_generator(selected_text, max_length=50, num_return_sequences=5, num_beams=5, early_stopping=True)
+         question=generated_questions[random_number]['generated_text']
+         result=question
+        #  result=selected_text + " İkinci işlem"
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      elif request.form['action'] == 'third_action':
          user = users_collection.find_one({'username': username})
          texts = user.get('texts', [])
          header="A title for you!"
          text_name = request.form['text_name']
-        #  random_number = random.randint(1, 3)         
-        #  generated_title = title_generator(selected_text, max_length=50, num_return_sequences=5, num_beams=5, early_stopping=True)
-        #  title=generated_title[random_number]['generated_text']
-        #  result=title
-         result=selected_text + " 3 işlem"
+         random_number = random.randint(1, 3)         
+         generated_title = title_generator(selected_text, max_length=50, num_return_sequences=5, num_beams=5, early_stopping=True)
+         title=generated_title[random_number]['generated_text']
+         result=title
+         #result=selected_text + " 3 işlem"
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      elif request.form['action'] == 'fourth_action':
          user = users_collection.find_one({'username': username})
          texts = user.get('texts', [])
          text_name = request.form['text_name']
          header="Spelling Correction"         
-         #result = spelling_correction(selected_text)[0]['generated_text']
-         result=selected_text + " 4 işlem"
+         result = spelling_correction(selected_text)[0]['generated_text']
+        #  result=selected_text + " 4 işlem"
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      elif request.form['action'] == 'fifth_action':
          user = users_collection.find_one({'username': username})
          texts = user.get('texts', [])
          text_name = request.form['text_name']
          header="Key Word"         
-         #result = keyword(selected_text)[0]['generated_text']
-         result=selected_text + " 5 işlem"
+         result = keyword(selected_text)[0]['generated_text']
+        #  result=selected_text + " 5 işlem"
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      elif request.form['action'] == 'sixth_action':
          user = users_collection.find_one({'username': username})
          texts = user.get('texts', [])
          text_name = request.form['text_name']
          header="Paraphrase"         
-         #result = paraphrase(selected_text)[0]['generated_text']
-         result=selected_text + " 6 işlem"
+         result = paraphrase(selected_text)[0]['generated_text']
+        #  result=selected_text + " 6 işlem"
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      elif request.form['action'] == 'seventh_action':
          user = users_collection.find_one({'username': username})
@@ -179,7 +179,7 @@ def profile():
          text_name = request.form['text_name']
          header="Grammar Correction"         
          result = grammar(selected_text)[0]['generated_text']
-         #result=selected_text + " 7 işlem"
+        #  result=selected_text + " 7 işlem"
          return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header,username=username, texts=texts,text_name=text_name)
      
     # Kullanıcı ile ilişkilendirilmiş metinleri veritabanından al
